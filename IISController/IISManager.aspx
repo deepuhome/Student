e038f68c6f0dae0fdce932e8e9bac999bc1b04cc
deepuinapp <deepusp.sree@gmail.com>
2015-02-10 11:13:32 +0530

    First Commit

---
+++ IISController/IISManager.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="IISManager.aspx.cs" Inherits="IISController.IISManager" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    <div>
+    
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" style="z-index: 1; left: 10px; top: 37px; position: absolute" Text="Button" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ IISController/IISManager.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace IISController
+{
+    public partial class IISManager : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = GetIISStatus();
+        }
+
+        private string GetIISStatus()
+        {
+            ServiceController controller = new ServiceController("W3SVC");
+            return controller.Status.ToString();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            ServiceController controller = new ServiceController("W3SVC");
+            if (controller.Status == ServiceControllerStatus.Running)
+                controller.Stop();
+            else if (controller.Status == ServiceControllerStatus.Stopped)
+                controller.Start();
+            else
+                controller.Stop();
+        }
+    }
+}
